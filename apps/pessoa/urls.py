cdef2c20d5341c50883ce8a8a25f4818b363a326
OlgaNabais <olgamlnabais@gmail.com>
2022-10-03 15:30:29 +0100

    delete

--- apps/pessoa/urls.py
+++ apps/pessoa/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import contacts, PessoaCreateView, PessoaListView, PessoaDetailView
+from .views import (contacts, PessoaCreateView, 
+                    PessoaListView, PessoaDetailView, 
+                    PessoaUpdateView, PessoaUpdateDetailView,
+                    PessoaDeleteView, PessoaDeleteDetailView)
 
 app_name = 'pessoa'
 
@@ -7,6 +10,11 @@ urlpatterns = [
      path('', contacts, name='contacts'),
      path('novo/', PessoaCreateView.as_view(), name='novo'),
      path('lista/', PessoaListView.as_view(), name='lista'),
-     path('detalhe/<int:pk>/', PessoaDetailView.as_view(), name='detalhe')
+     path('detalhe/<int:pk>/', PessoaDetailView.as_view(), name='detalhe'),
+     path('atualizar/<int:pk>/', PessoaUpdateView.as_view(), name='atualizar'),
+     path('detalhe/atualizar/<int:pk>/', PessoaUpdateDetailView.as_view(), name='detalhe-atualizar'),
+     path('confirmar/eliminar/<int:pk>/', PessoaDeleteView.as_view(), name='eliminar'),
+     path('detalhe/confirmar/eliminar/<int:pk>/', PessoaDeleteDetailView.as_view(), name='detalhe-eliminar')
+
 ]
     
--- apps/pessoa/views.py
+++ apps/pessoa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, ListView, DetailView
-from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from .models import Pessoa
 
 # Create your views here.
@@ -19,3 +19,26 @@ class PessoaListView(ListView):
 
 class PessoaDetailView(DetailView):
     model = Pessoa
+
+class PessoaUpdateView(UpdateView):
+    model = Pessoa
+    fields = ['nome', 'idade', 'email']
+    template_name = 'pessoa/pessoa_update.html'
+    success_url = reverse_lazy('pessoa:lista')
+
+class PessoaUpdateDetailView(UpdateView):
+    model = Pessoa
+    fields = ['nome', 'idade', 'email']
+    template_name = 'pessoa/pessoa_detail_update.html'
+    
+    def get_success_url(self):
+        return reverse('pessoa:detalhe-atualizar',kwargs={'pk': self.object.id})
+
+class PessoaDeleteView(DeleteView):
+    model = Pessoa
+    success_url: reverse_lazy('pessoa:lista')
+
+class PessoaDeleteDetailView(DeleteView):
+    model = Pessoa
+    template_name = 'pessoa/pessoa_detail_confirm_delete.html' 
+    success_url = reverse_lazy('pessoa:lista')
